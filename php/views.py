d939cee397a5fb6ce27b902620e9ae4704be36af
kpen <8e4e@mail.ru>
2016-11-03 21:00:53 -0400

    changes
    foo

--- php/views.py
+++ php/views.py
@@ -17,12 +17,12 @@ def firstwritephp(request):
   if request.POST.__contains__("prm1"):
    mystring = request.POST['prm1']
    b = mystring.encode('utf-8')
-   
+
    fd = os.open(str(acryl)+"/eee"+str(ent)+"_tmp.txt",os.O_RDWR|os.O_CREAT|os.O_TRUNC)
    ret = os.write(fd,b)
    os.close(fd)
   return HttpResponse("{\"param1\":\"111\",\"param2\":\"2222\",\"param3\":\"333\"}")
-  
+
 def nextwritephp(request):
   if request.POST.__contains__("prma"):
    acryl = int(request.POST['prma'])
@@ -35,13 +35,13 @@ def nextwritephp(request):
   if request.POST.__contains__("prm1"):
    mystring = request.POST['prm1']
    b = mystring.encode('utf-8')
-   
+
    fd = os.open(str(acryl)+"/eee"+str(ent)+"_tmp.txt",os.O_WRONLY|os.O_APPEND)
    ret = os.write(fd,b)
    os.close(fd)
-   
+
   return HttpResponse("{\"param1\":\"111\",\"param2\":\"2222\",\"param3\":\"333\"}")
-  
+
 def lastwritephp(request):
   if request.POST.__contains__("prma"):
    acryl = int(request.POST['prma'])
@@ -56,7 +56,13 @@ def lastwritephp(request):
   if os.path.isfile(str(acryl)+"/eee"+str(ent)+"_tmp.txt"):
    os.rename(str(acryl)+"/eee"+str(ent)+"_tmp.txt",str(acryl)+"/eee"+str(ent)+".txt")
   return HttpResponse("{\"param1\":\"111\",\"param2\":\"2222\",\"param3\":\"333\"}")
-  
+
+def nextimage(request):
+    pictures = load_all_pictures
+    # [Picture, Picture, Picture]
+    pictures.pop()
+    # LIFO algorithm
+
 def readphp(request):
  if request.POST.__contains__("prm0"):
   t22 = int(request.POST['prm0'])
